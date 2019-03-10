8dc461848fad455c89fa131a40cb379ce113f9d7
Dmitry Kostrikov <DaimonUA@gmail.com>
2019-03-11 01:30:03 +0200

    add HW.Abstraction

---
+++ ConsoleApp4/Human.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork.Abstraction
+{
+    abstract class Human
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public abstract void SayHello();
+        public override string ToString()
+        {
+            return Name+" from "+Country;
+        }
+    }
+    class Ukrainian:Human
+    {
+        public Ukrainian(string name)
+        {
+            Name = name;
+            this.Country = "UA";
+        }
+        public override void SayHello() => Console.WriteLine("Привіт!");
+    }
+    class American:Human
+    {
+        public American(string name)
+        {
+            Name = name;
+            Country = "USA";
+        }
+        public override void SayHello() => Console.WriteLine("Hi!");
+    }
+    class Italian:Human
+    {
+        public Italian(string name)
+        {
+            Name = name;
+            Country = "ITA";
+        }
+        public override void SayHello() => Console.WriteLine("Ciao!");
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HomeWork.Abstraction
+{
+//    Создайте абстрактный класс Человек, определите свойство Name и абстрактный метод SayHello().
+//От этого класса создайте несколько наследников, которые представляют национальность(украинец, американец...). Должно получиться так, что при вызове метода SayHello() выводилось приветствие на языке соответствующему национальности(Привіт, Hi...).
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human[] international = new Human[3];
+            international[0] = new Ukrainian("Taras");
+            international[1] = new American("Bob");
+            international[2] = new Italian("Luiggi");
+            foreach (Human p in international)
+            {
+                Console.Write(p.ToString()+" ");
+                p.SayHello();
+            }
+        }
+    }
+}
