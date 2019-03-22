a09a2d3e5f550328e0fbbfab0d74509085c65f81
Dmitry Kostrikov <DaimonUA@gmail.com>
2019-03-22 12:30:31 +0200

    +job with collecrion

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace TypeConversion
 {
@@ -11,16 +13,52 @@ namespace TypeConversion
     {
         static void Main(string[] args)
         {
-            Animal[] arrAnimal = new Animal[6];
-            arrAnimal[0] = new Dog { Name = "Шарик" };
-            arrAnimal[1] = new Cat { Name = "Кусака" };
-            arrAnimal[2] = new Cat { Name = "Ленивец" };
-            arrAnimal[3] = new Dog { Name = "Джек" };
-            arrAnimal[4] = new Cat { Name = "Черныш" };
-            arrAnimal[5] = new Dog { Name = "Арчи" };
-            foreach (Animal a in arrAnimal)
+            //Animal[] arrAnimal = new Animal[6];
+            //arrAnimal[0] = new Dog { Name = "Шарик" };
+            //arrAnimal[1] = new Cat { Name = "Кусака" };
+            //arrAnimal[2] = new Cat { Name = "Ленивец" };
+            //arrAnimal[3] = new Dog { Name = "Джек" };
+            //arrAnimal[4] = new Cat { Name = "Черныш" };
+            //arrAnimal[5] = new Dog { Name = "Арчи" };
+
+            //ArrayList arrAnimal = new ArrayList();
+            //arrAnimal.Add(new Dog { Name = "Шарик" });
+            //arrAnimal.Add(new Cat { Name = "Кусака" });
+            //arrAnimal.Add(new Cat { Name = "Ленивец" });
+            //arrAnimal.Add(new Dog { Name = "Джек" });
+            //arrAnimal.Add(new Cat { Name = "Черныш" });
+            //arrAnimal.Add(new Dog { Name = "Арчи" });
+
+            //List<Animal> arrAnimal = new List<Animal>();
+            //arrAnimal.Add(new Dog { Name = "Шарик" });
+            //arrAnimal.Add(new Cat { Name = "Кусака" });
+            //arrAnimal.Add(new Cat { Name = "Ленивец" });
+            //arrAnimal.Add(new Dog { Name = "Джек" });
+            //arrAnimal.Add(new Cat { Name = "Черныш" });
+            //arrAnimal.Add(new Dog { Name = "Арчи" });
+
+            //LinkedList<Animal> arrAnimal = new LinkedList<Animal>();
+            //arrAnimal.AddFirst(new Dog { Name = "Шарик" });
+            //arrAnimal.AddLast(new Cat { Name = "Кусака" });
+            //arrAnimal.AddLast(new Cat { Name = "Ленивец" });
+            //arrAnimal.AddLast(new Dog { Name = "Джек" });
+            //arrAnimal.AddLast(new Cat { Name = "Черныш" });
+            //arrAnimal.AddLast(new Dog { Name = "Арчи" });
+
+            Dictionary<string,Animal> arrAnimal = new Dictionary<string,Animal>();
+            arrAnimal.Add("dog_0",new Dog { Name = "Шарик" });
+            arrAnimal.Add("cat_0", new Cat { Name = "Кусака" });
+            arrAnimal.Add("cat_1", new Cat { Name = "Ленивец" });
+            arrAnimal.Add("dog_1", new Dog { Name = "Джек" });
+            arrAnimal.Add("cat_2", new Cat { Name = "Черныш" });
+            arrAnimal.Add("dog_2", new Dog { Name = "Арчи" });
+
+
+            foreach (KeyValuePair<string,Animal> a0 in arrAnimal)
             {
                 // Ваш код
+                Animal a = a0.Value;
+                Console.Write(a0.Key);
                 a.Bite();
                 if(a is Cat)
                 {
